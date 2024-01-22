40712ba0bf43355a2beb17f47e1e1748b8aec5ad
praveen-golagani <praveen.gyj@gmail.com>
2024-01-22 23:22:35 +0530

    alerts example

---
+++ src/test/java/alerts/AlertEx1.java
@@ -0,0 +1,31 @@
+package alerts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class AlertEx1 {
+
+	WebDriver driver = AppMain.getDriver();
+	String url = "https://rahulshettyacademy.com/AutomationPractice/";
+
+	void alertDemo(WebDriver driver) {
+		driver.get(url);
+		WebElement field = driver.findElement(By.id("name"));
+		field.sendKeys("Dhoni");
+		driver.findElement(By.id("alertbtn")).click();
+
+		String content = driver.switchTo().alert().getText();
+		System.out.println(content);
+		driver.switchTo().alert().accept();
+
+		driver.findElement(By.id("confirmbtn")).click();
+		String content1 = driver.switchTo().alert().getText();
+		System.out.println(content1);
+		driver.switchTo().alert().accept();
+
+		driver.findElement(By.id("confirmbtn")).click();
+		driver.switchTo().alert().dismiss();
+
+	}
+}
---
+++ src/test/java/alerts/AppMain.java
@@ -0,0 +1,24 @@
+package alerts;
+
+import org.openqa.selenium.WebDriver;
+
+import criotasks.LaunchBrowser;
+
+public class AppMain extends LaunchBrowser {
+
+	AppMain() {
+		super();
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		AppMain alt = new AppMain();
+		WebDriver driver = AppMain.getDriver();
+		
+		AlertEx1 ae1 = new AlertEx1();
+		ae1.alertDemo(driver);
+
+		Thread.sleep(3000);
+		alt.endTest();
+	}
+
+}
