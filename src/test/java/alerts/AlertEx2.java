1ff2baa8c7abd66a5b401a1b19b114366d96c8f2
praveen-golagani <praveen.gyj@gmail.com>
2024-01-23 08:39:10 +0530

    alert types exercise added

---
+++ src/test/java/alerts/AlertEx2.java
@@ -0,0 +1,36 @@
+package alerts;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AlertEx2 {
+
+	WebDriver driver = AppMain.getDriver();
+	String url = "https://the-internet.herokuapp.com/javascript_alerts";
+
+	public void alertTypes(WebDriver driver) {
+		driver.get(url);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
+		wait.until(ExpectedConditions.alertIsPresent());
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+
+		driver.findElement(By.xpath("//button[text()='Click for JS Confirm']")).click();
+		wait.until(ExpectedConditions.alertIsPresent());
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().dismiss();
+
+		
+		driver.findElement(By.xpath("//button[text()='Click for JS Prompt']")).click();
+				wait.until(ExpectedConditions.alertIsPresent());
+				System.out.println(driver.switchTo().alert().getText());
+				driver.switchTo().alert().sendKeys("Dhoni");
+				driver.switchTo().alert().accept();
+	}
+
+}
--- src/test/java/alerts/AppMain.java
+++ src/test/java/alerts/AppMain.java
@@ -14,8 +14,11 @@ public class AppMain extends LaunchBrowser {
 		AppMain alt = new AppMain();
 		WebDriver driver = AppMain.getDriver();
 		
-		AlertEx1 ae1 = new AlertEx1();
-		ae1.alertDemo(driver);
+//		AlertEx1 ae1 = new AlertEx1();
+//		ae1.alertDemo(driver);
+		
+		AlertEx2 ae2 = new AlertEx2();
+		ae2.alertTypes(driver);
 
 		Thread.sleep(3000);
 		alt.endTest();
